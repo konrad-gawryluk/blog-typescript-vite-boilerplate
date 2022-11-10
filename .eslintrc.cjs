28dc234020f3f52bf8ddd5f46633d99a13af0735
Konrad Gawryluk <konrad.gawryluk98@gmail.com>
2022-11-10 13:40:45 +0100

    Add husky, lint-staged, eslint and prettier configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'prettier',
+  ],
+};
---
+++ .lintstagedrc.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  '*.{css,ts,tsx,js,jsx}': 'prettier --write',
+  '*.{ts,tsx,js,jsx}': 'eslint --fix',
+};
